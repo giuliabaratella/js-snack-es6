10df9c4c6cde7f6802e296a2725e0cda1633f284
giuliabaratella <giuliabaratella7@gmail.com>
2023-10-24 16:45:31 +0200

    added arrays uppercaseNames + highGrades

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+// SNACK 1
 // Dwayne Johnson ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
 // Ci ha lasciato il nome del tavolo e la lista degli invitati in ordine di posto.
 // Ma la tipografia per stampare il tutto vuole che le mandiamo una lista di ospiti in un formato specifico, per cui dobbiamo fare in modo che ogni ospite sia un oggetto javascript che ha come attributi:
@@ -35,4 +37,74 @@ const listToPrint = TavoloVip.map((el,index)=>{
 console.log(listToPrint);
 
 
+
+// SNACK2 
+// Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame.
+// Questo è l'elenco degli studenti:
+// Id Name Grades
+// 213 Marco della Rovere 78
+// 110 Paola Cortellessa 96
+// 250 Andrea Mantegna 48
+// 145 Gaia Borromini 74
+// 196 Luigi Grimaldello 68
+// 102 Piero della Francesca 50
+// 120 Francesca da Polenta 84
+
+// 1) Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo
+//ES (Marco della Rovere => MARCO DELLA ROVERE);
+
+// 2) Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+
+// 3) Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+
+
+
+const students = [
+    {
+        id: 213,
+        name: 'Marco Della rovere',
+        grades: 78
+    },
+    {
+        id: 110,
+        name: 'Paola Cortellessa',
+        grades: 96
+    },
+    {
+        id: 250,
+        name: 'Andrea Mantegna',
+        grades: 48
+    },
+    {
+        id: 145,
+        name: 'Gaia Borromini',
+        grades: 74
+    },
+    {
+        id: 196,
+        name: 'Luigi Grimaldello',
+        grades: 68
+    },
+    {
+        id: 102,
+        name: 'Piero della Francesca',
+        grades: 50
+    },
+    {
+        id: 120,
+        name: 'Francesca da Polenta',
+        grades: 84
+    }
+]
+// array in uppercase 
+const uppercaseNames = students.map((el)=>{
+    return el.name.toUpperCase();
+})
+console.log(uppercaseNames);
+
+// array voti superiori a 70 
+const highGrades = students.filter((el)=>{
+    return el.grades >70;
+})
+console.log(highGrades);
 
