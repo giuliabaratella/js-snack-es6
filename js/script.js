36bcbe9e0bdf9259e4c9916a05fc8f89456ea63c
giuliabaratella <giuliabaratella7@gmail.com>
2023-10-25 18:36:21 +0200

    snack 4 / added array + random values for score and fouls

--- js/script.js
+++ js/script.js
@@ -161,3 +161,59 @@ bycicles.forEach((el,index)=>{
 console.log(minWeightBike);
 
 console.log(`La bici con il peso minore è ${minWeightBike.name}`)
+
+
+// SNACK 4
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const teams = [
+    {
+        name: 'Pippo',
+        score: 0,
+        fouls:0
+    },
+    {
+        name: 'Pluto',
+        score: 0,
+        fouls:0
+    },
+    {
+        name: 'Paperino',
+        score: 0,
+        fouls:0
+    },
+    {
+        name: 'Minnie',
+        score: 0,
+        fouls:0
+    },
+    {
+        name: 'Qui',
+        score: 0,
+        fouls:0
+    },
+    {
+        name: 'Quo',
+        score: 0,
+        fouls:0
+    },
+    {
+        name: 'Qua',
+        score: 0,
+        fouls:0
+    }
+]
+teams.forEach((el)=> {
+    el.score = getRndInteger(1, 100);
+    el.fouls = getRndInteger(1,20);
+})
+console.log (teams);
+
+
+// UTILITY 
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
